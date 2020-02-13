8a6d0f895ca5f63dc25b8506bc52fb020cded871
Felix <felix@onemorelevel.se>
2020-02-13 12:12:34 +0100

    Did some more changes

--- project/js/main.js
+++ project/js/main.js
@@ -1,3 +1,26 @@
+//$(document).ready(function(){
+//  getWeather();
+//})
+
+function getWeather(searchQuery){
+  var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=metric&appid="+apiKey;
+
+  $(".city").text("");
+  $(".temp").text(""); 
+
+  $.ajax(url,{success: function(data){
+    $(".city").text(data.name);
+    $(".temp").text(data.main.temp);
+  }, error: function(error){
+    $(".error-message").text("An error occured");
+  }})
+}
+
+function searchWeather() {
+  var searchQuery = $(".search").val();
+  getWeather(searchQuery);
+}
+
 function showPicture(){
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
